7764c4d6977e77a035e2328fbecbd386778c3534
han <maihanbrvt2410@gmail.com>
2023-05-31 20:40:09 +0700

    first commit

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+console.log(typeof 'aaaaa' )
+console.log(typeof false )
+console.log(typeof undefined)
+console.log(typeof null)
+
+let variable1
+let variable2
+let variable3
+let variable4
+
+let variable5=`break`
+let variable6="tomorrow"
+let variable7 =23
+let variable8='HaNoi'
+
+let firstName = 'han'
+let lastName = "do" 
+let isMarried = false
+let country ="VN"
+let age = 20
+console.log(firstName,lastName,isMarried,country,age)
+
+let firstname = 'han' ,lastname="do",ismarried= false ,Country='VN',Age="20"
+console.log(firstName,lastName,isMarried,country,age)
+
+let myAge = "I am 25 years old"
+let yourAge = "You are 30 years old"
+console.log(myAge,yourAge)
---
+++ variable.js
@@ -0,0 +1,6 @@
+let string='aaaaa'
+let boolean=false
+let undefined
+let emptyvalue=null
+
+console.log(string,boolean,undefined,emptyvalue)
